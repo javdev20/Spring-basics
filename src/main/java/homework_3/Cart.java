8ee9971b73fcf1320d78a7f2223e8e659bd0eb35
javdev20 <islam.qayumov07@gmail.com>
2022-04-18 17:14:31 +0300

    homework-3

---
+++ src/main/java/homework_3/Cart.java
@@ -0,0 +1,42 @@
+package homework_3;
+
+import org.codehaus.plexus.component.annotations.Component;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+@Scope("prototype")
+public class Cart {
+
+    private final Map<Product, Integer> cartMap = new HashMap<>();
+
+    public Map<Product, Integer> getCartMap() {
+        return cartMap;
+    }
+
+    public void addProduct(Product product, Integer quantity) {
+        if (product != null)
+            cartMap.merge(product, quantity, Integer::sum);
+    }
+
+    public void delProduct(Product product, Integer quantity) {
+        if (cartMap.containsKey(product)) {
+            if (quantity != null && cartMap.get(product).compareTo(quantity) > 0) {
+                cartMap.put(product, cartMap.get(product) - quantity);
+            } else {
+                cartMap.remove(product);
+            }
+        }
+    }
+
+    public BigDecimal getSum() {
+        BigDecimal sum = BigDecimal.valueOf(0);
+        for (Map.Entry<Product, Integer> entry : cartMap.entrySet()) {
+            sum = sum.add(entry.getKey().getPrice().multiply(BigDecimal.valueOf(entry.getValue())));
+        }
+        return sum;
+    }
+
+}
